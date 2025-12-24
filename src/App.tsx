7f9545c1ce828a246b984ba5f4d7a9aa0a17818c
KhFazliddin <81314108+KhFazliddin@users.noreply.github.com>
2025-12-24 08:44:32 +0500

    Remove logo import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./assesment/Header/Header";
 import Education from "./assesment/Education/Education";
